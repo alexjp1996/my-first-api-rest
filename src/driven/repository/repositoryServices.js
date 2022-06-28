9b1c038ea10cbccee53d111d9b747c49dbf673ad
Marlon Jimenez <alex_jp1996@hotmail.com>
2022-06-28 07:52:38 -0600

    fix some code

--- src/driven/repository/repositoryServices.js
+++ src/driven/repository/repositoryServices.js
@@ -1,6 +1,3 @@
-const { Message } = require('../../utils/Messages');
-
-const message = new Message();
 const AWS = require('aws-sdk');
 AWS.config.update( {
   region: 'us-east-1'
@@ -21,19 +18,19 @@ class repositoryServices {
             return await dynamodb.get(params).promise().then((response) => {
                 let res;
                 if(response.Item != undefined){
-                  res = message._200_OK;
+                  res = { "code": 200, "model": {"code": "OPERATION_SUCCESSFUL", "message": "Operación Exitosa"}};
                   res.model.data = response.Item;
                 } else {
-                    res = message._404_CAR_NOT_FOUND;
+                    res = { "code": 404, "model": {"code": "BRAND_NOT_FOUND", "message": "El carro no existe"}};
                 }
                 return res;
             }, (error) => {
-                    const resError = message._500_UNCONTROLLER_ERROR;
+                    const resError = { "code": 500, "model": {"code": "UNCONTROLLER_ERROR", "message": "Ocurrió un error desconocido"}};
                     resError.model.message = "Ocurrió el siguiente error: " + error.message;
                     return resError;
             });
         } catch(err){
-            const r = message._500_UNCONTROLLER_ERROR;
+            const r = { "code": 500, "model": {"code": "UNCONTROLLER_ERROR", "message": "Ocurrió un error desconocido"}};
             r.model.message = "Error desconocido: " + err.message;
             return r;
         }
@@ -51,19 +48,19 @@ class repositoryServices {
               return await dynamodb.get(params).promise().then((response) => {
                 let res;
                 if(response.Item != undefined){
-                    res = message._200_OK;
+                    res = { "code": 200, "model": {"code": "OPERATION_SUCCESSFUL", "message": "Operación Exitosa"}};
                     res.model.data = response.Item;
                 } else {
-                    res = message._404_CAR_NOT_FOUND;
+                    res = { "code": 404, "model": {"code": "BRAND_NOT_FOUND", "message": "El carro no existe"}};
                 }
                 return res;
               }, (error) => {
-                const resError = message._500_UNCONTROLLER_ERROR;
+                const resError = { "code": 500, "model": {"code": "UNCONTROLLER_ERROR", "message": "Ocurrió un error desconocido"}};
                 resError.model.message = "Ocurrió el siguiente error: " + error.message;
                 return resError;
               });
         } catch(err){
-            const r = message._500_UNCONTROLLER_ERROR;
+            const r = { "code": 500, "model": {"code": "UNCONTROLLER_ERROR", "message": "Ocurrió un error desconocido"}};
             r.model.message = "Ocurrió el siguiente error: " + err.message;
             return r;
         }
@@ -76,14 +73,14 @@ class repositoryServices {
                 Item: requestBody
             }
             return await dynamodb.put(params).promise().then(() => {
-                return message._200_OK;
+                return { "code": 200, "model": {"code": "OPERATION_SUCCESSFUL", "message": "Operación Exitosa"}};
             }, (error) => {
-                const resError = message._500_UNCONTROLLER_ERROR;
+                const resError = _500_UNCONTROLLER_ERROR = { "code": 500, "model": {"code": "UNCONTROLLER_ERROR", "message": "Ocurrió un error desconocido"}};
                 resError.model.message = "Ocurrió el siguiente error: " + error.message;
                 return resError;
             })
         } catch(err){
-            const r = message._500_UNCONTROLLER_ERROR;
+            const r = _500_UNCONTROLLER_ERROR = { "code": 500, "model": {"code": "UNCONTROLLER_ERROR", "message": "Ocurrió un error desconocido"}};
             r.model.message = "Ocurrió el siguiente error: " + err.message;
             return r;
         }
